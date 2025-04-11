d8975be5fb4ee293243f82e3aa4ad771bbf2ee80
larabislem <islamlarabi0@gmail.com>
2025-04-11 12:20:41 +0100

    feat: Add getExaminuser method to ExamenModel

--- models/examen_model.js
+++ models/examen_model.js
@@ -20,8 +20,16 @@ const ExamenModel = {
     getExamenById: async (id_examen) => {
         const result = await examen.query('SELECT * FROM "A9ra_brk".examen WHERE id_examen = $1', [id_examen]);
         return result.rows[0];
-    }
+    },
+   getExaminuser: async (id_user, id_examen) => {
+    const result = await client.query(
+        'INSERT INTO "A9ra_brk".user_examen (id_user, id_examen) VALUES ($1, $2) RETURNING *',
+        [id_user, id_examen]
+    );
+    return result.rows[0];
+},
 }
+    
 module.exports = ExamenModel;
 
 
