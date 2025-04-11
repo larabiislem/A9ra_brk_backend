a6a7a8ecb910f11085105eba7ae1ab1a4b5179da
larabislem <islamlarabi0@gmail.com>
2025-04-11 09:07:25 +0100

    feat: Add CRUD operations for Cours model

--- models/cours_model.js
+++ models/cours_model.js
@@ -1,47 +1,38 @@
 const Cours = require('../config/db');
 
 const Cours = {
-
-
-    async addplaylist(idplaylist, name_playlist, playlist_type, playlist_cat) {
-        const result = await client.query(
-            'INSERT INTO "A9ra_brk".playlist (id_playlist, name_playlist, playlist_type, playlist_cat) VALUES ($1, $2,$3,$4) RETURNING *',
-            [idplaylist, name_playlist, playlist_type, playlist_cat]
+    addCours: async ( id_cour,description_cour, date_update, duré, title_cour) => {
+        const result = await Cours.query(
+            'INSERT INTO "A9ra_brk".cours ("Id_cour", description_cour, date_update, duré, title_cour) VALUES ($1, $2, $3, $4,$5) RETURNING *',
+            [description_cour, date_update, duré, title_cour]
         );
         return result.rows[0];
-
     },
 
-     async getAllPlaylists() {
-        const result = await client.query('SELECT * FROM "A9ra_brk".playlist');
-        return result.rows; },
 
-    async getPlaylistById(idplaylist) {
-        const result = await client.query('SELECT * FROM "A9ra_brk".playlist WHERE id_playlist = $1', [idplaylist]);
+    getAllCours: async () => {
+        const result = await Cours.query('SELECT * FROM "A9ra_brk".cours');
+        return result.rows;
+    },
+    deleteCours: async (id_cour) => {
+        const result = await Cours.query('DELETE FROM "A9ra_brk".cours WHERE id_cour = $1 RETURNING *', [id_cour]);
         return result.rows[0];
     },
-
-    async deletePlaylist(idplaylist) {
-        const result = await client.query('DELETE FROM "A9ra_brk".playlist WHERE id_playlist = $1 RETURNING *', [idplaylist]);
+    getCoursById: async (id_cour) => {
+        const result = await Cours.query('SELECT * FROM "A9ra_brk".cours WHERE id_cour = $1', [id_cour]);
         return result.rows[0];
     },
+    updateCours: async (id_cour, description_cour, date_update, duré, title_cour) => {
+        const result = await Cours.query(
+            'UPDATE "A9ra_brk".cours SET description_cour = $2, date_update = $3, duré = $4, title_cour = $5 WHERE id_cour = $1 RETURNING *',
+            [id_cour, description_cour, date_update, duré, title_cour]
+        );
+        return result.rows[0];
+    }
+
 
     
-    async ajoutMotcledansplaylist(id_playlist, id){
-        const result = await client.query(
-            'INSERT INTO "A9ra_brk".playlist (id_playlist, id_motcle) VALUES ($1, $2) RETURNING *',
-            [id_playlist, id]
-        );
-        return result.rows[0];
-
-    },
-
-    async consultermotcledansplaylist(id_playlist, id){
-        const result = await client.query(
-            'SELECT id_motcle FROM "A9ra_brk".playlist WHERE id_playlist = $1',
-            [id_playlist]
-        );
-        return result.rows[0];
-    },
 
 }
+
+module.exports = Cours;
